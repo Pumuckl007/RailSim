678c17319fdb94fbd2c2adac9fa1c1634b2b08df
me <maximilian.paulsen@gmail.com>
2014-02-09 12:54:23 -0800

    removed jasmine

--- src/init.js
+++ src/init.js
@@ -1,1 +1,18 @@
-window.railsim = {};
+(function(){
+	window.Railsim = {};
+	Railsim.Windowmanager = {};
+	var screennames = [
+		"mainmenu"
+	]
+	var manager = Railsim.Windowmanager;
+	manager.screens = {};
+	for(var i = 0; i < screennames.length; i++){
+		var screen = document.createElement("iframe");
+		screen.src = "screens/" + screennames[i] + ".html";
+		document.getElementById("main").appendChild(screen);
+
+	}
+	manager.setVisible = function(screen){
+
+	}
+})();
